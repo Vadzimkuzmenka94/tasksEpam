f95283a431a37356e56714c3a2807834356d9796
37529 <VadzimKuzmenka@mail.ru>
2021-12-26 23:51:15 +0300

    Initial commit

---
+++ src/deed/StudentStatus.java
@@ -0,0 +1,14 @@
+package deed;
+
+/*
+    Возможные статусы студента:
+        выпущен(окончил университет)
+        зачислен(или учится)
+        отчислен
+ */
+
+public enum StudentStatus {
+    GRADUATED,
+    ENROLLED,
+    DEDUCTED
+}
